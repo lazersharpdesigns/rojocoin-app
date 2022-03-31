e509838a72a5ff4c68a809443e8ca169492cd578
lazersharpdesigns <admin@lazersharpdesigns.co.za>
2022-03-31 18:56:40 +0200

    init

---
+++ app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route index element={Home} />
+          <Route path="*" element={Home} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ app/src/pages/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home(props) {
+  return <div style={{ height: '100vh' }}>Home page</div>;
+}
+
+export default Home;
